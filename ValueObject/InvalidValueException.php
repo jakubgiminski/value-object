f5f6ef0f841959a5ca2f471f3f30654290263d74
Jakub Giminski <jakubgiminski@gmail.com>
2016-12-29 22:35:24 +0000

    Introduces InvalidValueException

---
+++ ValueObject/InvalidValueException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ValueObject;
+
+class InvalidValueException extends \InvalidArgumentException
+{
+    public static function tooShort(int $minLength): self
+    {
+        return new self("Value must be at least $minLength characters long.");
+    }
+
+    public static function tooLong(int $maxLength): self
+    {
+        return new self("Value must be no longer than $maxLength characters.");
+    }
+
+    public static function notEqualToAnyValidValues(): self
+    {
+        return new self('Value is invalid.');
+    }
+
+    public static function notInRange(): self
+    {
+        return new self('Value not in valid range.');
+    }
+}
