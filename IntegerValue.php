0abb1ac394e3055fc02e3b5433c82e3528fa1484
Jakub Giminski <jakubgiminski@gmail.com>
2016-12-29 20:55:38 +0000

    Introduces abstracts IntegerValue and StringValue

---
+++ IntegerValue.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace ValueObject;
+
+abstract class IntegerValue
+{
+    /** @var int */
+    protected $value;
+
+    /** @var array */
+    protected $validValues = [];
+
+    /**
+     * @param int $value
+     * @throws \InvalidArgumentException
+     */
+    protected function __construct(int $value)
+    {
+        $this->validate($value);
+        $this->value = $value;
+    }
+
+    /**
+     * @return int
+     */
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param IntegerValue $integerValue
+     * @return bool
+     */
+    public function isEqual(IntegerValue $integerValue): bool
+    {
+        return $this->getValue() === $integerValue->getValue();
+    }
+
+    /**
+     * @param int $value
+     * @throws \InvalidArgumentException
+     */
+    protected function validate(int $value)
+    {
+        if (in_array($value, $this->validValues) === false) {
+            throw new \InvalidArgumentException;
+        }
+    }
+}
---
+++ StringValue.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ValueObject;
+
+abstract class StringValue
+{
+    /** @var string */
+    protected $value;
+
+    /** @var array */
+    protected $validValues = [];
+
+    /**
+     * @param string $value
+     * @throws \InvalidArgumentException
+     */
+    protected function __construct(string $value)
+    {
+        $this->validate($value);
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param StringValue $stringValue
+     * @return bool
+     */
+    public function isEqual(StringValue $stringValue): bool
+    {
+        return $this->getValue() === $stringValue->getValue();
+    }
+
+    /**
+     * @param string $value
+     * @throws \InvalidArgumentException
+     * @return self
+     */
+    protected function validate(string $value)
+    {
+        if (in_array($value, $this->validValues) === false) {
+            throw new \InvalidArgumentException;
+        }
+    }
+}
